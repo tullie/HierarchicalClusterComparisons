9621e530f0e291551716c5bfde6a05210eeb9951
Tullie Murrell <tulliemurrell@gmail.com>
2016-06-16 18:40:45 +0930

    Add plotting code

--- vis/plot.py
+++ vis/plot.py
@@ -3,5 +3,10 @@ import numpy as np
 from matplotlib import pyplot
 
 data = np.loadtxt(sys.argv[1]);
-pyplot.scatter(data[:,0], data[:,1], c=data[:, 2])
+
+color_map = ["red", "green", "blue"]
+labels = data[:, 2]
+label_colors = [ color_map[int(label)] for label in labels ]
+
+pyplot.scatter(data[:,0], data[:,1], c=label_colors)
 pyplot.show()
